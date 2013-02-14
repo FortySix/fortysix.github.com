ec42426e55779f6d9610a17572a5c2a62c20b489
achariam <matthew.achariam@gmail.com>
2013-02-13 22:56:27 -0500

    fixed banner padding

--- js/app.js
+++ js/app.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-	$(".essence").fitText(1.5, {minFontSize: '20px', maxFontSize: '40px'});
+	$(".essence").fitText(1.5, {minFontSize: '18px', maxFontSize: '40px'});
 });
