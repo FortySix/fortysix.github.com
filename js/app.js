c80cf755088c5341dec4dc471bd0e6a9154ab92e
achariam <matthew.achariam@gmail.com>
2013-02-15 01:13:02 -0500

    Added Twinkling Stars

--- js/app.js
+++ js/app.js
@@ -1,3 +1,52 @@
 $(document).ready(function() {
 	$(".essence").fitText(1.6, {minFontSize: '18px', maxFontSize: '45px'});
+
+	isStarsVisible = true;
+	var windowWidth = $('.banner').width();
+	var windowHeight = $('.banner').height();
+	var $stars;
+
+
+	$stars = $('#stars');
+
+
+	function twinkle(v)
+	{
+		v.attr('class','twinkle');
+		var randX = Math.floor(Math.random() * windowWidth);
+		var randY = Math.floor(Math.random() * windowHeight);
+		if(isStarsVisible)
+		{
+			v.css({'transform': 'translate3d(' + randX + 'px,'+randY+'px,0)',
+					'webkitTransform': 'translate3d(' + randX + 'px,'+randY+'px,0)',
+					'mozTransform': 'translate3d(' + randX + 'px,'+randY+'px,0)',
+					'msTransform': 'translate3d(' + randX + 'px,'+randY+'px,0)',
+					'oTransform': 'translate3d(' + randX + 'px,'+randY+'px,0)'});
+		}
+		setTimeout(function() {
+			v.attr('class','');
+			setTimeout(twinkle,0,v);
+		}, 2000);
+	}
+
+	for(var i = 0; i < 20; i ++)
+	{
+		$stars.append('<div class="star" />');
+	}
+
+	$.each($('.star'), function(i, v) {
+		var randIn =  i * 100 + 2000;
+		setTimeout(function() {
+			var $v = $(v);
+			twinkle($v);
+		}, randIn);
+	});
+
+	$(window).bind('resize', function() {
+		windowWidth = $('.banner').width();
+		windowHeight = $('.banner').height();
+	});
+
+
 });
+
