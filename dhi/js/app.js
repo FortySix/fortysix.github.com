be04b055acd52d13cb293ed1d7615a440395e261
achariam <matthew.achariam@gmail.com>
2013-05-16 23:01:13 -0400

    Dental Health International
    Added subfolder for demo

---
+++ dhi/js/app.js
@@ -0,0 +1,10 @@
+  $(".banner").backstretch([
+    "img/roomcloseup.jpg",
+    "img/frontoffice.jpg",
+    "img/reception.jpg",   
+    "img/signoutside.jpg"
+ 
+  ], {duration: 3000, fade: 750});
+
+
+  $(".tagline").fitText(0.8, {minFontSize: '18px', maxFontSize: '68px'});
