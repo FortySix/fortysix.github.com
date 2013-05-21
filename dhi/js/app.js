8d574931360376a4fb2e6fddbb4d0ff8882f4a46
achariam <matthew.achariam@gmail.com>
2013-05-21 19:35:14 -0400

    Added Our Team Page

--- dhi/js/app.js
+++ dhi/js/app.js
@@ -1,3 +1,4 @@
+ if ($('.home').length > 0){
   $(".banner").backstretch([
     "img/roomcloseup.jpg",
     "img/frontoffice.jpg",
@@ -5,6 +6,40 @@
     "img/signoutside.jpg"
  
   ], {duration: 3000, fade: 750});
+}
+
+ else if ($('.services').length > 0){
+ $(".banner").backstretch("img/roomcloseup.jpg"); 
+
+(function($) {
+    
+       $('.accordion dd').hide();
+       $('.accordion dt a').click(function(){
+          if ($(this).hasClass('selected')) {
+               $(this).removeClass('selected');
+               $(this).parent().next().slideUp();
+          } else {
+               $('.accordion dt a').removeClass('selected');
+               $(this).addClass('selected');
+               $('.accordion dd').slideUp();
+               $(this).parent().next().slideDown();
+          }
+          return false;
+       });
+
+})(jQuery);
+
+  
+ }
+
+ else if ($('.ourteam').length > 0){
+ $(".banner").backstretch("img/group.jpg");   
+ }
 
 
-  $(".tagline").fitText(0.8, {minFontSize: '18px', maxFontSize: '68px'});
+ else if ($('.contactus').length > 0){
+ $(".banner").backstretch("img/group.jpg");   
+ }
+
+
+  $(".tagline").fitText(0.8, {minFontSize: '18px', maxFontSize: '58px'});
