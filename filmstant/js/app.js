c88d244774fab4af8fea6e243e7879c304884028
achariam <matthew.achariam@gmail.com>
2013-06-10 12:41:04 -0400

    Added more bios

--- filmstant/js/app.js
+++ filmstant/js/app.js
@@ -18,8 +18,6 @@ $(document).ready(function() {
 });
 
 
-
-
 // callback for when we get back the results
 function searchCallback(data) {
 
@@ -45,7 +43,9 @@ function searchCallback(data) {
 
    $('.movielist').append('<li><div class="postercolumn"><img src="' + movie.posters.detailed + '" width="180px" height="261px"/><div class="blueer"><a href="http://www.youtube.com/embed?listType=search&list=' + movie.title + ' trailer" target="_blank"><span class="icon" aria-hidden="true" data-icon="&#9654;"></span>   trailer</a></div><a href="http://www.google.com/movies?hl=en&q=' + movie.title + '" target="_blank"><span class="icon" aria-hidden="true" data-icon="&#128340;"></span>   showtimes</a></div><div class="infocolumn"><div class="titlebox"><h2>' + movTitle + '</h2></div><div class="reviewbox"><p>' + movie.critics_consensus + '</p><h6> ' + movie.release_dates.theater + ' &#183; ' + movie.runtime + ' min &#183; Rated ' + movie.mpaa_rating + '</h6></div><div class="scores"><div class="criticbox"><h3>' + movie.ratings.critics_score + '</h3><p>CRITICS</p></div><div class="audiencebox"><h3>' + movie.ratings.audience_score + '</h3><p>AUDIENCE</p></div></div></div></li>');
 
+	console.log(movie.ratings.audience_score);
  });
+
 
 }
 
