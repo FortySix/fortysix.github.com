fc8bd988dacea8914913178d0a0d7a9a3ac8b0d3
achariam <matthew.achariam@gmail.com>
2013-06-09 22:32:42 -0400

    Progress

--- filmstant/js/app.js
+++ filmstant/js/app.js
@@ -24,10 +24,25 @@ function searchCallback(data) {
 
 
  var movies = data.movies;
+ var movTitle;
 
  $.each(movies, function(index, movie) {
 
-   $('.movielist').append('<li><h2>' + movie.title + '</h2> <img src="' + movie.posters.detailed + '" /> <h3>' + movie.ratings.critics_score + '%</h3><h3>' + movie.ratings.audience_score + '%</h3><p>' + movie.critics_consensus + '</p><h4>' + movie.runtime + ' min.</h4><h4>' + movie.release_dates.theater + '</h4><h4>Rated ' + movie.mpaa_rating + '</h4><a href="http://www.google.com/movies?hl=en&q=' + movie.title + '" target="_blank">Showtimes</a> <a href="http://www.youtube.com/embed?listType=search&list=' + movie.title + ' trailer" target="_blank">Trailer</a></li>');
+
+ 	// To Do List
+ 	//Javascript needs to be refactored/tweaked heavily and html moved out.
+ 	//Format Date better
+
+ 	if (movie.title.length > 23) { 
+ 		var length = 20;
+		movTitle = movie.title.substring(0,length);
+		movTitle = movTitle+'...';
+ 	}
+ 	else{
+ 		movTitle = movie.title;
+ 	}
+
+   $('.movielist').append('<li><div class="postercolumn"><img src="' + movie.posters.detailed + '" width="180px" height="260px"/><div class="blueer"><a href="http://www.youtube.com/embed?listType=search&list=' + movie.title + ' trailer" target="_blank"><span class="icon" aria-hidden="true" data-icon="&#9654;"></span>   trailer</a></div><a href="http://www.google.com/movies?hl=en&q=' + movie.title + '" target="_blank"><span class="icon" aria-hidden="true" data-icon="&#128340;"></span>   showtimes</a></div><div class="infocolumn"><h2>' + movTitle + '</h2><h3>' + movie.ratings.critics_score + ' critics</h3><h3>' + movie.ratings.audience_score + ' audience</h3><div class="reviewbox"><p>' + movie.critics_consensus + '</p><h6> ' + movie.release_dates.theater + ' &#183; ' + movie.runtime + ' min &#183; Rated ' + movie.mpaa_rating + '</h6></div></div></li>');
 
  });
 
