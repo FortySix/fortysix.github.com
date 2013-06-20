a544333df06519b94eff5346964aff3a9f7457de
achariam <matthew.achariam@gmail.com>
2013-06-20 12:44:50 -0400

    Top

--- filmstant/js/app.js
+++ filmstant/js/app.js
@@ -1,6 +1,19 @@
 $.backstretch("img/topbg.jpg");
 
 
+$(window).scroll(function() {
+    if ($(this).scrollTop()) {
+        $('#toTop').fadeIn();
+    } else {
+        $('#toTop').fadeOut();
+    }
+});
+
+$("#toTop").click(function() {
+    $("html, body").animate({scrollTop: 0}, 1000);
+ });
+
+
 $(document).ready(function() {
 
 
